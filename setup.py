7f00c4294ae13d8dc7482c36d43199dcc410aa46
PriyanshuDey23 <priyanshudey.ds@gmail.com>
2024-10-05 14:39:43 +0530

    Setup added

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Remain Common in every Project
+
+import setuptools
+
+
+with open("README.md","r",encoding="utf-8") as f: # Open in write mode
+    long_description=f.read()
+
+__version__="0.0.0"
+
+REPO_NAME= "US-Visa-Approval-System"
+AUTHOR_USER_NAME="PriyanshuDey23"
+SRC_REPO= "US-Visa-Approval-System"
+AUTHOR_EMAIL="priyanshudey.ds@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="It is a Machine Learning Problem Statement",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/PriyanshuDey23/US-Visa-Approval-System",
+    project_urls={
+        "Bug Tracker":f"https://github.com/PriyanshuDey23/US-Visa-Approval-System/issues"
+    },
+    
+    packages=setuptools.find_packages()
+
+
+
+)
