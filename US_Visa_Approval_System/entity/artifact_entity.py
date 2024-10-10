ae55a4947e375797416dfc1cb77755b8543c935b
PriyanshuDey23 <priyanshudey.ds@gmail.com>
2024-10-10 21:37:53 +0530

    Data Validation Stage added

--- US_Visa_Approval_System/entity/artifact_entity.py
+++ US_Visa_Approval_System/entity/artifact_entity.py
@@ -7,4 +7,13 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionArtifact:
     trained_file_path: str
-    test_file_file: str
+    test_file_Path: str
+
+
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    drift_report_file_path: str
+
+
--- US_Visa_Approval_System/pipline/training_pipeline.py
+++ US_Visa_Approval_System/pipline/training_pipeline.py
@@ -2,12 +2,14 @@ from US_Visa_Approval_System.exception import USvisaException
 from US_Visa_Approval_System.logger import logging
 import sys
 from US_Visa_Approval_System.components.data_ingestion import DataIngestion
-from US_Visa_Approval_System.entity.config_entity import DataIngestionConfig
-from US_Visa_Approval_System.entity.artifact_entity import  DataIngestionArtifact
+from US_Visa_Approval_System.components.data_validation import DataValidation
+from US_Visa_Approval_System.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from US_Visa_Approval_System.entity.artifact_entity import  DataIngestionArtifact,DataValidationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_comfig=DataIngestionConfig()
+        self.data_validation_config=DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -27,12 +29,38 @@ class TrainPipeline:
             raise USvisaException(e, sys) from e
         
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+        
+
     def run_pipeline(self) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             
 
         except Exception as e:
