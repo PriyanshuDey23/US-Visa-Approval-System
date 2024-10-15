a940992ebe71b86c42efbdd3d00f7180ee7b1737
PriyanshuDey23 <priyanshudey.ds@gmail.com>
2024-10-15 16:06:25 +0530

    App Added

--- US_Visa_Approval_System/pipline/training_pipeline.py
+++ US_Visa_Approval_System/pipline/training_pipeline.py
@@ -6,8 +6,9 @@ from US_Visa_Approval_System.components.data_validation import DataValidation
 from US_Visa_Approval_System.components.data_transformation import DataTransformation
 from US_Visa_Approval_System.components.model_trainer import ModelTrainer
 from US_Visa_Approval_System.components.model_evaluation import ModelEvaluation
-from US_Visa_Approval_System.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig
-from US_Visa_Approval_System.entity.artifact_entity import  DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact
+from US_Visa_Approval_System.components.model_pusher import ModelPusher
+from US_Visa_Approval_System.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig
+from US_Visa_Approval_System.entity.artifact_entity import  DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact,ModelPusherArtifact
 
 class TrainPipeline:
     def __init__(self):
@@ -16,6 +17,7 @@ class TrainPipeline:
         self.data_transformation_config=DataTransformationConfig()
         self.model_trainer_config=ModelTrainerConfig()
         self.model_evaluation_config = ModelEvaluationConfig()
+        self.ModelPusherConfig=ModelPusherConfig()
 
       
 
@@ -110,6 +112,18 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys)
 
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
 
     
 
@@ -127,6 +141,10 @@ class TrainPipeline:
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
                                                                     model_trainer_artifact=model_trainer_artifact)
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
         except Exception as e:
             raise USvisaException(e, sys) from e
         
